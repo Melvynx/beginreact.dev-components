3219ee27afaf333eaa83a38751fabf99e5a57978
Melvyn M <56388157+Melvynx@users.noreply.github.com>
2022-09-06 08:47:26 +0200

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import "./chore/styles/index.css";
 import "./chore/styles/prose.css";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
